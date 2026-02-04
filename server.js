1b3c42254ade1be18bca19a4a451c63b831dc76c
Ryan Augusto Pestana <ryanpestana2005@gmail.com>
2026-02-04 11:40:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const db = mysql.createPool({
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
+    port: Number(process.env.MYSQLPORT),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
